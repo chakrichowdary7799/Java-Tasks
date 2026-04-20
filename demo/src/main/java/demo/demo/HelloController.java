bb8368a92da47121acf00550bca6200241023787
chakrichowdary7799 <chakri.kota07@gmail.com>
2026-04-20 14:10:22 +0530

    demo

--- demo/src/main/java/demo/demo/HelloController.java
+++ demo/src/main/java/demo/demo/HelloController.java
@@ -10,8 +10,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/api")
 public class HelloController {
     @GetMapping("/hello")
-    public String sayHello(@RequestParam String param) {
+    public String sayHello(@RequestParam(required = false) String param) {
         return "Hello World";
     }
     
 }
+
+
+
