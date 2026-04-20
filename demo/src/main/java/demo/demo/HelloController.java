44171388740b68500302451f516a5592a99d1e3c
chakrichowdary7799 <chakri.kota07@gmail.com>
2026-04-20 13:05:04 +0530

    initial commit

---
+++ demo/src/main/java/demo/demo/HelloController.java
@@ -0,0 +1,17 @@
+package demo.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam String param) {
+        return "Hello World";
+    }
+    
+}
