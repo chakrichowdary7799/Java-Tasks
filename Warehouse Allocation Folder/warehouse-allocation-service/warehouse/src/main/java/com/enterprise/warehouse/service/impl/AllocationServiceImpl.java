f402569ced89d19df6e628c662d6d6e3cc96e219
Sagarchowdary3 <sagarchowdary99511@gmail.com>
2026-05-19 18:21:10 +0530

    Warehouse Allocation

--- Warehouse Allocation Folder/warehouse-allocation-service/warehouse/src/main/java/com/enterprise/warehouse/service/impl/AllocationServiceImpl.java
+++ Warehouse Allocation Folder/warehouse-allocation-service/warehouse/src/main/java/com/enterprise/warehouse/service/impl/AllocationServiceImpl.java
@@ -1,7 +1,7 @@
 package com.enterprise.warehouse.service.impl;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
