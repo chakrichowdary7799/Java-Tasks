a50dda2b38f2f9fd5fda3e0652a733b92cf066ef
chakrichowdary7799 <chakri.kota07@gmail.com>
2026-06-29 11:32:49 +0530

    Enterprise Order System

---
+++ Coupon Discount Folder/coupon-engine/src/main/java/com/enterprise/coupon_engine/CouponEngineApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.coupon_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponEngineApplication.class, args);
+	}
+
+}
